38e58d8c87b4c7ef2489070569290f26b83d90ef
Davi Rodrigues <30713947+daviirodrig@users.noreply.github.com>
2022-02-10 19:32:39 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,14 @@
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 function proj {Set-Location D:/Projects}
+function new-proj {
+	param (
+		$name
+	)
+	Set-Location "D:/Projects"
+	mkdir $name
+	cd $name
+	code .
+}
 function mitm {
 	$tmp = Get-Location
 	Set-Location "C:\Program Files (x86)\mitmproxy\bin\"
