58eb24b10af9145e5192c7817e7ee2df1918e631
Davi Rodrigues <30713947+daviirodrig@users.noreply.github.com>
2022-03-19 22:33:45 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,34 @@
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-function proj {Set-Location D:/Projects}
-function which {
-	param ($cmd)
-	Get-Command $cmd | Select-Object -ExpandProperty Definition
+Set-PSReadLineOption -PredictionSource History
+Remove-Item alias:cat -Force
+Remove-Item alias:ls -Force
+function proj { Set-Location D:/Projects }
+function which ($c) { Get-Command $cmd | Select-Object -ExpandProperty Definition }
+function yt-mp3 ($c) { youtube-dl --extract-audio --audio-format mp3 $p }
+function cat ($c) { bat -pp --paging=never $c }
+function ls ($c) { PowerColorLS $c }
+# https://github.com/mrpond/BlockTheSpot
+function spotify-ad { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -RemoveAdPlaceholder" }
+
+function new-proj ($name) {
+  Set-Location "D:/Projects"
+  mkdir $name
+  Set-Location $name
+  code .
 }
-function new-proj {
-	param (
-		$name
-	)
-	Set-Location "D:/Projects"
-	mkdir $name
-	cd $name
-	code .
-}
-function spotify-ad {
-	# https://github.com/mrpond/BlockTheSpot
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -RemoveAdPlaceholder"
-}
+
 function mitm {
-	$tmp = Get-Location
-	Set-Location "C:\Program Files (x86)\mitmproxy\bin\"
-	./run.ps1 mitmproxy
-	Set-Location $tmp
+  $tmp = Get-Location
+  Set-Location "C:\Program Files (x86)\mitmproxy\bin\"
+  ./run.ps1 mitmproxy
+  Set-Location $tmp
 }
+
 function mitmweb {
-	$tmp = Get-Location
-	Set-Location "C:\Program Files (x86)\mitmproxy\bin\"
-	./run.ps1 mitmweb
-	Set-Location $tmp
+  $tmp = Get-Location
+  Set-Location "C:\Program Files (x86)\mitmproxy\bin\"
+  ./run.ps1 mitmweb
+  Set-Location $tmp
 }
+
 Invoke-Expression (&starship init powershell)
