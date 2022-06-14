e024688b716672ed55cc9cf1ba6905efa5032d60
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2022-06-13 23:38:44 -0300

    update

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "Running apt update"
-sudo apt update
+sudo apt-get update
 
 echo "Installing bat"
-sudo apt install -y bat
+sudo apt-get install -y bat
 
 echo "Installing zsh"
-sudo apt install -y zsh
+sudo apt-get install -y zsh
 zsh --version
 
 echo "Installing oh-my-zsh"
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Changing default shell to zsh"
+chsh -s /usr/bin/zsh codespace
 
 echo "Installing zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
