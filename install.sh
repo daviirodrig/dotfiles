61833db39f29a8088164849ed776834dc63af0fc
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2022-06-14 00:05:23 -0300

    update

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 echo "Copy .zshrc to HOME"
 cp -f .zshrc ~/
 
-echo "Changing default shell to zsh"
-chsh -s /usr/bin/zsh
-
 echo "Installing zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Changing default shell to zsh"
+sudo chsh -s $(which zsh) codespace
