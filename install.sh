cc950508683e313c75dcabf3bc1118080c3f211a
Davi Rodrigues <30713947+daviirodrig@users.noreply.github.com>
2022-08-26 03:41:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ echo "Installing zsh"
 sudo apt-get install -y zsh
 zsh --version
 
+echo "Installing bat"
+sudo apt-get install -y bat
+batcat --version
+
 echo "Installing oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -18,5 +22,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo "Installing zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "Changing default shell to zsh"
-sudo chsh -s $(which zsh) codespace
+#echo "Changing default shell to zsh"
+#sudo chsh -s $(which zsh) codespace
